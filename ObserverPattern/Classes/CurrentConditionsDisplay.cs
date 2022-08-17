2dd9e8e1308f2d28e3b19f43dd19d7d390112205
npalli12 <95227394+npalli12@users.noreply.github.com>
2022-08-17 07:58:22 -0400

    Created CurrentConditionsDisplay.cs

---
+++ ObserverPattern/Classes/CurrentConditionsDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ObserverPattern.Interfaces;
+
+namespace ObserverPattern.Classes;
+
+public class CurrentConditionsDisplay : IObserver, IDisplayElement
+{
+    private float _temperature;
+    private float _humidity;
+    private WeatherData _weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData)
+    {
+        _weatherData = weatherData;
+        _weatherData.RegisterObserver(this);
+    }
+    public void Update(float temp, float humidity, float pressure)
+    {
+        _temperature = temp;
+        _humidity = humidity;
+        Display();
+    }
+
+    public void Display()
+    {
+        var displayStr = "Current Conditions : " + _temperature
+                                                 + "F degrees and "
+                                                 + _humidity + "% humidity";
+        Console.WriteLine(displayStr);
+    }
+}
